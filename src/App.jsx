b9a7a6f09cba3868e25bd2baf810afc5f28f48ce
mosesmulumba <mulumbamoses94@gmail.com>
2023-07-04 22:20:21 +0300

    button styles: added arrows and changed the colors for the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,74 @@
-import { useState , useEffect } from 'react'
-import axios from 'axios';
-import ReactPaginate from 'react-paginate';
-import  "./App.css";
+import {useState, useEffect} from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+import "./App.css";
+import {HiOutlineArrowSmRight, HiOutlineArrowSmLeft} from "react-icons/hi";
 
-export default function App(){
+export default function App() {
+  const [offset, setOffset] = useState(1);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0);
 
-  const [ offset, setOffset ] = useState(1);
-  const [ data , setData ] = useState([]);
-  const [ perPage ] = useState(10);
-  const [ pageCount , setPageCount ] = useState(0);
-
-  const getData = async() =>{
-    try{
-      const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/photos"
+      );
       const data = response.data;
 
-        const slice = data.slice(offset, offset + perPage)
-        const postData = slice.map(data => <div key={data.id}>
+      const slice = data.slice(offset, offset + perPage);
+      const postData = slice.map((data) => (
+        <div key={data.id}>
           <p>{data.title}</p>
           <img src={data.thumnailUrl} alt="" />
-        </div>)
+        </div>
+      ));
 
       setData(postData);
-      setPageCount(Math.ceil(data.length/perPage))
-    }catch(erorr){
-      console.log('Failed to fetch the data')
+      setPageCount(Math.ceil(data.length / perPage));
+    } catch (erorr) {
+      console.log("Failed to fetch the data");
     }
-  }
+  };
 
-  const handlePageClick = (e) =>{
+  const handlePageClick = (e) => {
     const selectedPage = e.selected;
     setOffset(selectedPage);
-  }
+  };
 
-  useEffect(() =>{
+  useEffect(() => {
     getData();
-  },[offset]);
+  }, [offset]);
 
   return (
-    <div className='app'>
+    <div className="app">
       {data}
       <ReactPaginate
-        previousLabel={"prev"}
-        nextLabel={"next"}
+        previousLabel={
+          <span style={{alignItems: "center", justifyItems: "center",display:'flex'}}>
+            <HiOutlineArrowSmLeft />
+            <i>Prev</i>
+          </span>
+          
+        }
+        nextLabel={
+          <span style={{alignItems: "center", justifyItems: "center",display:'flex'}}>
+            <i>Next</i>
+            <HiOutlineArrowSmRight />
+          </span>
+        }
         breakLabel={"..."}
         breakClassName={"break-me"}
         pageCount={pageCount}
         marginPagesDisplayed={2}
         pageRangeDisplayed={5}
         onPageChange={handlePageClick}
-        containerClassName={'pagination'}
-        subContainerClassName={'pages_pagination'}
-        activeClassName={'active'}
-        className='paginator'
-        />
+        containerClassName={"pagination"}
+        subContainerClassName={"pages_pagination"}
+        activeClassName={"active"}
+        className="paginator"
+      />
     </div>
-  )
+  );
 }
